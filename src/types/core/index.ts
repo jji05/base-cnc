a1b02d5d6655d9d01dec2a403e3744d128b8cc17
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2026-03-16 23:00:36 +1300

    feat(types): add base cnc config type

---
+++ src/types/core/index.ts
@@ -0,0 +1,10 @@
+/**
+ * The base configuration object for core
+ */
+export type BaseCncConfig = {
+  /**
+   * An optional prefix, default to blank string
+   * @default ""
+   */
+  commandPrefix?: string;
+};
